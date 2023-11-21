77817a7400d7a5b18602065c98053d1e70aa252c
bruce <psybe1@nottingham.ac.uk>
2023-11-21 14:39:43 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/button-has-type */
+// <reference path="path/types.d.ts" />
+import FaSun from "./assests/images/FaSun.png";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h3>react-typescript boilerplate!</h3>
+      <h5>testing lint stage!!</h5>
+
+      <img src={FaSun} alt="FaSun" />
+      
+
+    </div>
+  );
+};
+
+export default App;
