94b1d28403f7a357d235522186c7d3b43ec142a0
bruce <psybe1@nottingham.ac.uk>
2023-11-23 11:15:57 +0000

    import mui & location input field

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@
 // <reference path="path/types.d.ts" />
 import FaSun from "./assests/images/FaSun.png";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+
+  const [location, setLocation] = useState('London');
+  
+  
+  
   return (
     <div>
       <h3>react-typescript boilerplate!</h3>
       <h5>testing lint stage!!</h5>
+
 
       <img src={FaSun} alt="FaSun" />
       
