07da81b10737a2e5aa103a5690c455e7e63f0ef5
Bruce Estivil <baestivil@gmail.com>
2024-05-20 15:25:54 +0100

    fix: rebuilt server

---
+++ dist/api/server.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch';
+const API_KEY = 'b471c1780bd24c85ad5190222232012';
+export default async (req, res) => {
+    const requestUrl = req.url || '';
+    const fullUrl = new URL(requestUrl, `http://${req.headers.host}`);
+    const { pathname, searchParams } = fullUrl;
+    const location = searchParams.get('location');
+    if (!location) {
+        res.status(400).send('Location parameter is required');
+        return;
+    }
+    try {
+        if (pathname === '/api/weather') {
+            const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&aqi=no`;
+            const response = await fetch(url);
+            const data = await response.json();
+            res.send(data);
+        }
+        else if (pathname === '/api/autocomplete') {
+            const url = `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${location}&aqi=no`;
+            const response = await fetch(url);
+            const data = await response.json();
+            res.send(data);
+        }
+        else {
+            res.status(404).send('Not Found');
+        }
+    }
+    catch (error) {
+        console.error('Failed to fetch data:', error);
+        res.status(500).send('Failed to fetch data');
+    }
+};
